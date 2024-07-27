cf2f73b23f374974930a0c3411e0d38c6fca7bd4
sanchezjuliomarcelo <sanchezjuliomarcelo@gmail.com>
2024-07-27 17:45:29 -0300

    2

--- public/script.js
+++ public/script.js
@@ -4,13 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
         const response = await fetch('/login');
         const result = await response.json();
         const messageDiv = document.getElementById('message');
-  
+        
         // Limpiar las clases de mensaje previas
         messageDiv.classList.remove('alert-success', 'alert-danger');
   
         if (result.success) {
+          // Mostrar mensaje de autenticación correcta
           messageDiv.textContent = result.message;
           messageDiv.classList.add('alert', 'alert-success');
+  
+          // Guardar el token en localStorage y sessionStorage
+          const token = JSON.parse(result.token).token;
+          localStorage.setItem('andreaniToken', token);
+          sessionStorage.setItem('andreaniToken', token);
         } else {
           messageDiv.textContent = `Error: ${result.message}`;
           messageDiv.classList.add('alert', 'alert-danger');
