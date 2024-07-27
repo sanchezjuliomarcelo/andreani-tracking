8556aac5e4e6d1624eac8380706ec61fac9745e1
sanchezjuliomarcelo <sanchezjuliomarcelo@gmail.com>
2024-07-27 18:20:00 -0300

    3

--- public/script.js
+++ public/script.js
@@ -27,5 +27,38 @@ document.addEventListener('DOMContentLoaded', () => {
         messageDiv.classList.add('alert', 'alert-danger');
       }
     });
+  
+    document.getElementById('trackingForm').addEventListener('submit', async (event) => {
+      event.preventDefault();
+      const trackingNumber = document.getElementById('trackingNumber').value;
+      const token = sessionStorage.getItem('andreaniToken');
+  
+      if (!token) {
+        alert('Debe autenticarse primero.');
+        return;
+      }
+  
+      const myHeaders = new Headers();
+      myHeaders.append("x-authorization-token", token);
+  
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+      };
+  
+      try {
+        const response = await fetch(`https://apis.andreani.com/v2/ordenes-de-envio/${trackingNumber}`, requestOptions);
+        const result = await response.text();
+        console.log('API response received:', result);
+  
+        const resultDiv = document.getElementById('result');
+        resultDiv.textContent = result;
+      } catch (error) {
+        console.error('Error:', error);
+        const resultDiv = document.getElementById('result');
+        resultDiv.textContent = `Error: ${error.message}`;
+      }
+    });
   });
   
